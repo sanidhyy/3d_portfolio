935073d4c17f467da0670e19daeb8e41f5065347
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2023-11-13 12:55:05 +0000

    plane animation added

--- src/models/Plane.jsx
+++ src/models/Plane.jsx
@@ -1,11 +1,23 @@
-import { useGLTF } from "@react-three/drei";
+import { useEffect, useRef } from "react";
+import { useAnimations, useGLTF } from "@react-three/drei";
+
 import planeScene from "../assets/3d/plane.glb";
 
 const Plane = ({ isRotating, ...props }) => {
+  const ref = useRef();
   const { scene, animations } = useGLTF(planeScene);
+  const { actions } = useAnimations(animations, ref);
+
+  useEffect(() => {
+    if (isRotating) {
+      actions["Take 001"].play();
+    } else {
+      actions["Take 001"].stop();
+    }
+  }, [actions, isRotating]);
 
   return (
-    <mesh {...props}>
+    <mesh {...props} ref={ref}>
       <primitive object={scene} />
     </mesh>
   );
