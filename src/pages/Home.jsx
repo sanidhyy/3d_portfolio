d172f0f51379103f4910469572ae1453b2de3052
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2023-11-13 11:45:35 +0000

    island lightening added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -33,11 +33,13 @@ const Home = () => {
         camera={{ near: 0.1, far: 1000 }}
       >
         <Suspense fallback={<Loader />}>
-          <directionalLight />
-          <ambientLight />
-          <pointLight />
-          <spotLight />
-          <hemisphereLight />
+          <directionalLight position={[1, 1, 1]} intensity={2} />
+          <ambientLight intensity={0.5} />
+          <hemisphereLight
+            skyColor="#b1e1ff"
+            groundColor="#000000"
+            intensity={1}
+          />
 
           <Island
             position={islandPosition}
