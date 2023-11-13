73e7703250735b2ebee2bb6a80b25f02fcc2c5b3
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2023-11-13 11:31:26 +0000

    island added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,53 @@
+import { Suspense } from "react";
+import { Canvas } from "@react-three/fiber";
+
+import Loader from "../components/Loader";
+import Island from "../models/island";
+
 const Home = () => {
-  return <div>Home</div>;
+  const adjustIslandForScreenSize = () => {
+    let screenScale = null;
+    let screenPosition = [0, -6.5, -43];
+    let rotation = [0.1, 4.7, 0];
+
+    if (window.innerWidth < 768) {
+      screenScale = [0.9, 0.9, 0.9];
+    } else {
+      screenScale = [1, 1, 1];
+    }
+
+    return [screenScale, screenPosition, rotation];
+  };
+
+  const [islandScale, islandPosition, islandRotation] =
+    adjustIslandForScreenSize();
+
+  return (
+    <section className="w-full h-screen relative">
+      {/* <div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-center">
+        POPUP
+      </div> */}
+
+      <Canvas
+        className="w-full h-screen bg-transparent"
+        camera={{ near: 0.1, far: 1000 }}
+      >
+        <Suspense fallback={<Loader />}>
+          <directionalLight />
+          <ambientLight />
+          <pointLight />
+          <spotLight />
+          <hemisphereLight />
+
+          <Island
+            position={islandPosition}
+            scale={islandScale}
+            rotation={islandRotation}
+          />
+        </Suspense>
+      </Canvas>
+    </section>
+  );
 };
 
 export default Home;
