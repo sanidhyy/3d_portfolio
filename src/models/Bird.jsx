da722eb5b04d31345295f8f10d8cab7944286503
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2023-11-13 13:32:25 +0000

    bird animations implemented

--- src/models/Bird.jsx
+++ src/models/Bird.jsx
@@ -1,11 +1,39 @@
-import { useGLTF } from "@react-three/drei";
+import { useRef, useEffect } from "react";
+import { useAnimations, useGLTF } from "@react-three/drei";
+import { useFrame } from "@react-three/fiber";
 
 import birdScene from "../assets/3d/bird.glb";
 
 const Bird = () => {
+  const birdRef = useRef();
   const { scene, animations } = useGLTF(birdScene);
+  const { actions } = useAnimations(animations, birdRef);
+
+  useEffect(() => {
+    actions["Take 001"].play();
+  }, [actions]);
+
+  useFrame(({ clock, camera }) => {
+    // Update the Y position simulate the flight moving in a sin wave
+    birdRef.current.position.y = Math.sin(clock.elapsedTime) * 0.2 + 2;
+
+    if (birdRef.current.position.x > camera.position.x + 10) {
+      birdRef.current.rotation.y = Math.PI;
+    } else if (birdRef.current.position.x < camera.position.x - 10) {
+      birdRef.current.rotation.y = 0;
+    }
+
+    if (birdRef.current.rotation.y === 0) {
+      birdRef.current.position.x += 0.01;
+      birdRef.current.position.z -= 0.01;
+    } else {
+      birdRef.current.position.x -= 0.01;
+      birdRef.current.position.z += 0.01;
+    }
+  });
+
   return (
-    <mesh position={[-5, 2, 1]} scale={[0.003, 0.003, 0.003]}>
+    <mesh position={[-5, 2, 1]} scale={[0.003, 0.003, 0.003]} ref={birdRef}>
       <primitive object={scene} />
     </mesh>
   );
