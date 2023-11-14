de2033bc067258ddd0f79e8dfe3b8fc56ddae47c
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2023-11-14 04:42:31 +0000

    [pwa] configured

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,51 @@
 import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  base: "./",
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "prompt",
+      includeAssets: ["favicon.ico", "apple-touch-icon.png"],
+      manifest: {
+        name: "Sanidhya - 3D Portfolio",
+        short_name: "Sanidhya",
+        description: "Amazing 3D Portfolio in React using Three.js",
+        icons: [
+          {
+            src: "/android-chrome-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "/apple-touch-icon.png",
+            sizes: "180x180",
+            type: "image/png",
+            purpose: "apple touch icon",
+          },
+          {
+            src: "/maskable_icon.png",
+            sizes: "120x120",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+        theme_color: "#8796ff",
+        background_color: "#ffffff",
+        display: "standalone",
+        scope: "/",
+        start_url: "/",
+        orientation: "portrait",
+      },
+    }),
+  ],
   assetsInclude: ["**/*.glb"],
 });
